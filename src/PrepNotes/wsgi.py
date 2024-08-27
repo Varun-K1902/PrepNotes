122c4fe0c6dd581ec480b236c53486adb348db48
Varun-K1902 <varun.k1902@gmail.com>
2024-08-27 21:04:24 +0530

    update wsgi

--- src/PrepNotes/wsgi.py
+++ src/PrepNotes/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PrepNotes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.PrepNotes.settings')
 
 application = get_wsgi_application()
