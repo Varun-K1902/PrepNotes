77b3e48098dec7008ddf082851908d7558589031
Varun-K1902 <varun.k1902@gmail.com>
2024-08-28 11:19:18 +0530

    base

--- PrepNotes/settings.py
+++ PrepNotes/settings.py
@@ -170,7 +170,7 @@ TINYMCE_DEFAULT_CONFIG = {
     "undo redo | bold italic underline strikethrough | fontselect fontsizeselect customerfontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment code typography",
   "plugins":
     "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table powerpaste advcode help wordcount spellchecker typography",
-  "selector": "textarea",
+  "selector": '.tinymce', 
 };
 
 django_heroku.settings(locals())
